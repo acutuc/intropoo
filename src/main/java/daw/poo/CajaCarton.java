482b8d483848d1b74d74a575f6523c7f88b52379
acutuc <acutuc94@gmail.com>
2021-11-23 14:24:26 +0100

    Se sube ejercicio POO a git

---
+++ src/main/java/daw/poo/CajaCarton.java
@@ -0,0 +1,60 @@
+
+package daw.poo;
+
+/**
+ *
+ * @author acutuc
+ */
+public class CajaCarton {
+    
+
+    private double ancho;
+    private double alto;
+    private double largo;
+    private double peso;
+
+    public CajaCarton(double ancho, double alto, double largo, double peso) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.largo = largo;
+        this.peso = peso;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public void abrir(){
+        
+    }
+    public void cerrar(){
+        
+    }
+}
