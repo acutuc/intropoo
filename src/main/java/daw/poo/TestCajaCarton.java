9a3fae70166faee154235205e284f8219e2f4332
acutuc <acutuc94@gmail.com>
2021-11-23 18:10:59 +0100

    Se añade ejercicio para corregir en clase

--- src/main/java/daw/poo/TestCajaCarton.java
+++ src/main/java/daw/poo/TestCajaCarton.java
@@ -12,8 +12,10 @@ public class TestCajaCarton {
         CajaCarton cajaGrande = new CajaCarton(250, 400, 500, 200);
         CajaCarton cajaChica = new CajaCarton(25, 30, 15, 17);
         
-        System.out.println("Caja Grande --> " + cajaGrande.toString());
-        System.out.println("Caja Chica --> " + cajaChica.toString());
+        System.out.println("Caja Grande con toString --> " + cajaGrande.toString());
+        System.out.println("Caja Chica con toString--> " + cajaChica.toString());
+        System.out.println("Caja Grande con get -->" + "Alto: " + cajaGrande.getAlto() + "mm " + "Ancho: " + cajaGrande.getAncho() + "mm " + "Largo: " + cajaGrande.getLargo() + "mm " + "Peso: " + cajaGrande.getPeso() + "gr.");
+        System.out.println("Caja Crande con get -->" + "Alto: " + cajaChica.getAlto() + "mm " + "Ancho: " + cajaChica.getAncho() + "mm " + "Largo: " + cajaChica.getLargo() + "mm " + "Peso: " + cajaChica.getPeso() + "gr.");
         
         
     }
---
+++ src/main/java/daw/poo/Utilidades.java
@@ -0,0 +1,26 @@
+
+package daw.poo;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Utilidades {
+    
+    public static void Utilidades(){
+        
+            
+     
+    }
+            public static int generarAleatorios(){
+                Random aleatorio = new Random();
+                int numAleatorio = aleatorio.nextInt(10) + 1;
+                return numAleatorio;
+            }
+            public static void leerEnteros (){
+                System.out.println();
+            }
+
+}
