7204359210d46b23c59af0db2f331f1f99562545
acutuc <acutuc94@gmail.com>
2021-12-01 14:41:57 +0100

    Se corrige (a medias) Cafetera

--- src/main/java/daw/poo/cafetera/Cafetera.java
+++ src/main/java/daw/poo/cafetera/Cafetera.java
@@ -1,14 +1,19 @@
 package daw.poo.cafetera;
 
-/**
- *
- * @author acutuc
- */
+//Partes de una clase
+//Definición (public class Nombre)
 public class Cafetera {
+//(Utilidad que tienen public o private en la definición de una clase).
+//Encapsulamiento, qué es.
 
+    //Atributos encapsulados, campos o propiedades
     private double capacidadMaxima;
     private double cantidadActual;
 
+    //Métodos:
+    //Constructores sobrecargados (igual nombre, distintos parámetros) o no
+    //Getters y Setters
+    //toString
     /*Constructor predeterminado, sin parámetros: establece la capacidad máxima en 1000 (c.c.) y la cantidad actual
     en cero (cafetera vacía).*/
     public Cafetera() {
@@ -18,39 +23,57 @@ public class Cafetera {
 
     /*Constructor, que recibe sólo la capacidad máxima de la cafetera; inicializa la cantidad actual de café
     igual a la capacidad máxima (cafetera llena).*/
-    public double Cafetera(double capacidadMaxima) {
-        this.cantidadActual = capacidadMaxima;
-        return capacidadMaxima;
+    public Cafetera(double capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        if (capacidadMaxima < 10) {
+            this.capacidadMaxima = 10;
+        }
+        this.cantidadActual = this.capacidadMaxima;
     }
 
     /*Constructor, que recibe la capacidad máxima y la cantidad actual. Si la cantidad actual que se pasa es mayor
     que la capacidad máxima de la cafetera, se ajustará la cantidad actual a la capacidad máxima.*/
     public Cafetera(double capacidadMaxima, double cantidadActual) {
-        if (cantidadActual > capacidadMaxima) {
-            this.cantidadActual = capacidadMaxima;
-        }
         this.capacidadMaxima = capacidadMaxima;
-        this.cantidadActual = cantidadActual;
+        this.cantidadActual = Math.abs(cantidadActual);
+
+        if (capacidadMaxima < 10) {
+            this.capacidadMaxima = 10;
+        }
+
+        if (this.cantidadActual > this.capacidadMaxima) {
+            this.cantidadActual = this.capacidadMaxima;
+        }
     }
 
     //llenarCafetera(): hace que la cantidad actual sea igual a la capacidad.
-    public double llenarCafetera() {
-        return this.cantidadActual = this.capacidadMaxima;
+    public void llenarCafetera() {
+        this.cantidadActual = this.capacidadMaxima;
     }
 
     /*servirTaza(double cantidadAServir): simula la acción de servir una taza con la capacidad indicada.
     Si la cantidad actual de café “no alcanza” para llenar la taza, se sirve lo que quede.*/
-    public double servirTaza(double cantidadAServir) {
-        if (cantidadAServir < cantidadActual) {
-            cantidadAServir = cantidadActual;
+    public void servirTaza(double cantidadAServir) {
+        double cantidadPositiva = Math.abs(cantidadAServir);
+        this.cantidadActual -= cantidadPositiva;
+        if (this.cantidadActual < 0) {
+            this.cantidadActual = 0;
         }
-        return cantidadAServir;
     }
-    
+
     //vaciarCafetera(): pone la cantidad de café actual en cero.
-    public double vaciarCafetera(){
-        cantidadActual = 0;
-        return cantidadActual;
+    public void vaciarCafetera() {
+        this.cantidadActual = 0;
+    }
+
+    /*agregarCafe(double cantidadAgregar): añade a la cafetera la cantidad de café indicada, teniendo en
+    cuenta que la cafetera no puede rebosar.*/
+    public void agregarCafe(double cantidadAgregar) {
+        if ((cantidadAgregar + this.cantidadActual) > this.capacidadMaxima) {
+            cantidadAgregar = 0;
+        } else {
+            cantidadAgregar = cantidadAgregar + this.cantidadActual;
+        }
     }
 
     public double getCapacidadMaxima() {
